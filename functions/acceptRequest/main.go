764484eb4df02ad652246c7fc7641682780da134
Bill Glover <bill@billglover.co.uk>
2018-09-01 15:51:34 +0100

    New: log request path for valid requests
    * Validate inbound requests
    * Log request path and body

--- functions/acceptRequest/main.go
+++ functions/acceptRequest/main.go
@@ -2,79 +2,37 @@ package main
 
 import (
 	"context"
-	"crypto/hmac"
-	"crypto/sha256"
-	"encoding/hex"
 	"fmt"
 	"net/http"
 	"os"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ssm"
-	"github.com/pkg/errors"
+	ss "github.com/billglover/bbot/secrets"
+	bot "github.com/billglover/bbot/slackbot"
 )
 
 var slackClientID string
 var slackClientSecret string
 var slackSigningSecret string
 
-// Response is of type APIGatewayProxyResponse
-type Response events.APIGatewayProxyResponse
-
-// Request is of type APIGatewayProxyRequest
-type Request events.APIGatewayProxyRequest
-
 // Handler is our lambda handler invoked by the `lambda.Start` function call
-func handler(ctx context.Context, req Request) (Response, error) {
+func handler(ctx context.Context, req bot.Request) (bot.Response, error) {
 
-	if validateRequest(req, slackClientSecret) == false {
-		resp := Response{StatusCode: http.StatusBadRequest}
-		return resp, nil
+	if bot.ValidateRequest(req, slackSigningSecret) == false {
+		return bot.ErrorResponse("invalid request, check request signature", http.StatusBadRequest)
 	}
 
-	resp := Response{StatusCode: http.StatusAccepted}
+	fmt.Println("Path:", req.Path)
+	fmt.Println("Body:", req.Body)
+
+	resp := bot.Response{
+		StatusCode: http.StatusAccepted,
+		Headers:    map[string]string{"Content-Type": "application/json"}}
 	return resp, nil
 }
 
-func validateRequest(req Request, secret string) bool {
-	if req.HTTPMethod != http.MethodPost {
-		return false
-	}
-
-	ts, ok := req.Headers["X-Slack-Request-Timestamp"]
-	if ok == false {
-		return false
-	}
-
-	sig, ok := req.Headers["X-Slack-Signature"]
-	if ok == false {
-		return false
-	}
-
-	if checkHMAC(req.Body, ts, sig, secret) == false {
-		return false
-	}
-
-	return true
-}
-
-// CheckHMAC reports whether msgHMAC is a valid HMAC tag for msg.
-func checkHMAC(body, timestamp, msgHMAC, key string) bool {
-	msgHMAC = msgHMAC[3:]
-	msg := "v0:" + timestamp + ":" + body
-	hash := hmac.New(sha256.New, []byte(key))
-	hash.Write([]byte(msg))
-
-	expectedKey := hash.Sum(nil)
-	actualKey, _ := hex.DecodeString(msgHMAC)
-	return hmac.Equal(expectedKey, actualKey)
-}
-
 func main() {
-	secrets, err := getSecrets([]string{
+	secrets, err := ss.GetSecrets([]string{
 		"/bbot/env/SLACK_CLIENT_ID",
 		"/bbot/env/SLACK_CLIENT_SECRET",
 		"/bbot/env/SLACK_SIGNING_SECRET",
@@ -89,25 +47,4 @@ func main() {
 	slackSigningSecret = secrets["/bbot/env/SLACK_SIGNING_SECRET"]
 
 	lambda.Start(handler)
-}
-
-func getSecrets(keys []string) (map[string]string, error) {
-	svc := ssm.New(session.New())
-
-	paramsIn := ssm.GetParametersInput{
-		Names:          aws.StringSlice(keys),
-		WithDecryption: aws.Bool(true),
-	}
-
-	paramsOut, err := svc.GetParameters(&paramsIn)
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to get parameters from AWS parameter store")
-	}
-
-	secrets := make(map[string]string, len(paramsOut.Parameters))
-	for _, p := range paramsOut.Parameters {
-		secrets[*p.Name] = *p.Value
-	}
-
-	return secrets, nil
 }
--- functions/acceptRequest/main_test.go
+++ functions/acceptRequest/main_test.go
@@ -1,25 +1,79 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"reflect"
 	"testing"
+
+	bot "github.com/billglover/bbot/slackbot"
 )
 
-func TestValidateRequest(t *testing.T) {
+var tcs = []struct {
+	Name     string
+	Request  bot.Request
+	Response bot.Response
+}{
+	{
+		Name: "valid request signature",
+		Request: bot.Request{
+			Body:       "token=xyzz0WbapA4vBCDEFasx0q6G&team_id=T1DC2JH3J&team_domain=testteamnow&channel_id=G8PSS9T3V&channel_name=foobar&user_id=U2CERLKJA&user_name=roadrunner&command=%2Fwebhook-collect&text=&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT1DC2JH3J%2F397700885554%2F96rGlfmibIGlgcZRskXaIFfN&trigger_id=398738663015.47445629121.803a0bc887a14d10d2c447fce8b6703c",
+			HTTPMethod: http.MethodPost,
+			Headers: map[string]string{
+				"X-Slack-Request-Timestamp": "1531420618",
+				"X-Slack-Signature":         "v0=a2114d57b48eac39b9ad189dd8316235a7b4a8d21a10bd27519666489c69b503"},
+		},
+		Response: bot.Response{
+			StatusCode:      http.StatusAccepted,
+			Body:            "",
+			Headers:         map[string]string{"Content-Type": "application/json"},
+			IsBase64Encoded: false,
+		},
+	},
+	{
+		Name: "invalid request signature",
+		Request: bot.Request{
+			Body:       "token=xyzz0WbapA4vBCDEFasx0q6G&team_id=T1DC2JH3J&team_domain=testteamnow&channel_id=G8PSS9T3V&channel_name=foobar&user_id=U2CERLKJA&user_name=roadrunner&command=%2Fwebhook-collect&text=&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT1DC2JH3J%2F397700885554%2F96rGlfmibIGlgcZRskXaIFfN&trigger_id=398738663015.47445629121.803a0bc887a14d10d2c447fce8b6703c",
+			HTTPMethod: http.MethodPost,
+			Headers: map[string]string{
+				"X-Slack-Request-Timestamp": "1531420618",
+				"X-Slack-Signature":         "v0=b2114d57b48eac39b9ad189dd8316235a7b4a8d21a10bd27519666489c69b503"},
+		},
+		Response: bot.Response{
+			StatusCode:      http.StatusBadRequest,
+			Body:            `{"status":"400","message":"invalid request, check request signature"}`,
+			Headers:         map[string]string{"Content-Type": "application/json"},
+			IsBase64Encoded: false,
+		},
+	},
+}
 
-	req := Request{
-		Body:       "token=xyzz0WbapA4vBCDEFasx0q6G&team_id=T1DC2JH3J&team_domain=testteamnow&channel_id=G8PSS9T3V&channel_name=foobar&user_id=U2CERLKJA&user_name=roadrunner&command=%2Fwebhook-collect&text=&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT1DC2JH3J%2F397700885554%2F96rGlfmibIGlgcZRskXaIFfN&trigger_id=398738663015.47445629121.803a0bc887a14d10d2c447fce8b6703c",
-		HTTPMethod: http.MethodPost,
-		Headers: map[string]string{
-			"X-Slack-Request-Timestamp": "1531420618",
-			"X-Slack-Signature":         "v0=a2114d57b48eac39b9ad189dd8316235a7b4a8d21a10bd27519666489c69b503"},
-	}
+func TestHandler(t *testing.T) {
 
-	secret := "8f742231b10e8888abcd99yyyzzz85a5"
+	slackSigningSecret = "8f742231b10e8888abcd99yyyzzz85a5"
 
-	valid := validateRequest(req, secret)
-	if valid == false {
-		t.Fail()
+	for _, tc := range tcs {
+		t.Run(tc.Name, func(t *testing.T) {
+			ctx := context.Background()
+			resp, err := handler(ctx, tc.Request)
+
+			if err != nil {
+				t.Error(err)
+			}
+
+			if got, want := resp.StatusCode, tc.Response.StatusCode; got != want {
+				t.Errorf("unexpected StatusCode: got %d, want %d", got, want)
+			}
+
+			if got, want := resp.Body, tc.Response.Body; got != want {
+				t.Errorf("unexpected Body:\n  got : %s\n  want: %s", got, want)
+			}
+
+			if got, want := resp.Headers, tc.Response.Headers; reflect.DeepEqual(got, want) == false {
+				t.Errorf("unexpected headers:\n  got : %+v\n  want: %+v", got, want)
+			}
+
+		})
 	}
 
 }
